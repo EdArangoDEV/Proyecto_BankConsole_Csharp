5b4224c6bf2f3a7b9d33c70328e4a5fdfabb7cf2
Randol Gutierrez <randolg@GBODEGAS.INTERNO>
2024-09-24 09:54:30 -0600

    Se agregan archivos para compilar en VS code

--- BankSolution/BankConsole/Program.cs
+++ BankSolution/BankConsole/Program.cs
@@ -1,11 +1,11 @@
 ﻿using BankConsole;
 
-User James = new User(1,"James", "james@gmail.com",4000);
+User James = new User(1,"James", "james@gmail.com", 4000);
 James.SetBalance(-50);
 
 Console.WriteLine(James.ShowData());
 
-User Pedro = new User(2,"Pedro","pedro@hotmail.com",2500);
+User Pedro = new User(2,"Pedro","pedro@hotmail.com", 2500);
 
 //Console.WriteLine(Pedro.ShowData());
 
