553e5ca74d9b638e30b557833397833c41efc6ae
Randol Gutierrez <randolg@GBODEGAS.INTERNO>
2024-09-25 17:41:40 -0600

    agregando cambios a develop

--- BankSolution/BankConsole/obj/Debug/net8.0/BankConsole.AssemblyInfo.cs
+++ BankSolution/BankConsole/obj/Debug/net8.0/BankConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8eac7baf318fa3e66f5d37c797712c1f737c7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e5d5eba455379a42a85f8abbe134996dbf4ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
